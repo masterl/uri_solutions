83c9891c994a94e05c90e6a3b85925d18546dacd
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2019-10-11 17:28:47 -0700

    Add 1_beginner/1008 solution

---
+++ 1_beginner/1008/solution.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+employee_number = int(input())
+hours_worked = int(input())
+hourly_salary = float(input())
+
+print("NUMBER = %d" % (employee_number))
+print("SALARY = U$ %.2f" % (hours_worked * hourly_salary))
